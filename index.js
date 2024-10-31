2085095b3fd380c4ae7349e2e27f10548d20481e
Leonardo <leonardo_dl@hotmail.com.ar>
2024-10-31 14:43:18 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require ('express')
 const app = express();
 app.get('/', (req, res) => {
-res.send('Talento Tech Deploy');
+res.send('http://crisal-seven.vercel.app');
 })
 const PORT = process.env.PORT || 3000
 app.listen(PORT, () => console.log('http://localhost:${PORT}'));
