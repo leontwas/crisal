4c24eb9121117e36037a29f444aa6e45f0afea58
Leonardo <leonardo_dl@hotmail.com.ar>
2024-11-03 03:02:40 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,22 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 
 // Sirve archivos estáticos desde el directorio 'public'
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Redirigir a la URL de Netlify
 app.get('/', (req, res) => {
-  res.redirect('crisal-seguridad.netlify.app/');
+  res.redirect('http://crisal-seguridad.netlify.app/');
 });
 
+// Ruta para servir el index.html desde la raíz
+app.get('/index.html', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+// Iniciar el servidor
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Servidor ejecutándose en http://localhost:${PORT}`));
+app.listen(PORT, () => {
+  console.log(`Servidor ejecutándose en http://localhost:${PORT}`);
+});
