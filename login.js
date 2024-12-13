c91e49896eecdb66ff2da01b057af48e0a194f28
Leonardo <leonardo_dl@hotmail.com.ar>
2024-12-13 17:04:26 -0300

    Update login.js

--- login.js
+++ login.js
@@ -1,32 +1,31 @@
-// login.js
+document.getElementById('loginForm').addEventListener('submit', async (event) => {
+    event.preventDefault();
 
-const loginForm = document.getElementById('loginForm');
-loginForm.addEventListener('submit', (event) => {
-  event.preventDefault();
+    const username = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
 
-  const username = document.getElementById('username').value;
-  const password = document.getElementById('password').value;
+    const loginData = { username, password };
 
-  fetch('http://localhost:3000/api/login', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify({ username, password }),
-  })
-    .then((response) => response.json())
-    .then((data) => {
-      if (data.token) {
-        // Almacenar el token en el localStorage o sessionStorage
-        localStorage.setItem('token', data.token);
+    try {
+        const response = await fetch('http://localhost:3000/api/login', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(loginData),
+        });
 
-        // Redirigir a la página administrador.html
-        window.location.href = 'administrador.html';
-      } else {
-        alert('Usuario o contraseña incorrectos');
-      }
-    })
-    .catch((error) => {
-      console.error('Error al intentar iniciar sesión:', error);
-    });
+        const result = await response.json();
+
+        if (response.ok) {
+            // Redirigir a la página admin.html si la autenticación es exitosa
+            window.location.href = 'admin.html';
+        } else {
+            console.error('Error al intentar iniciar sesión:', result.message);
+            alert('Error: ' + result.message);
+        }
+    } catch (error) {
+        console.error('Error al intentar iniciar sesión:', error);
+        alert('Error de conexión');
+    }
 });
