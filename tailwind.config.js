739a97b3fa0940f0301c011f48c6e859d0614948
Leonardo <leonardo_dl@hotmail.com.ar>
2024-12-17 18:55:58 -0300

    revierto los cambios

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./*.html",                     // Archivos HTML en la raíz del proyecto
-    "./**/*.html",                  // Archivos HTML en subcarpetas
-    "./assets/js/**/*.js",          // Archivos JavaScript en la carpeta js
-    "./components/**/*.{js,ts,jsx,tsx}", // Otros archivos con clases de Tailwind (React/Vue/TS)
-  ],
-  prefix: 'tw-', // Prefijo opcional para evitar conflictos de clases
+  content: [],
   theme: {
-    extend: {
-      // Puedes agregar extensiones personalizadas aquí
-    },
+    extend: {},
   },
   plugins: [],
 }
+
